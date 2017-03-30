50d5902190a831262b15198efda400923acff6f1
SALeon <developleon@gmail.com>
2017-03-30 23:41:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/serg/custonsearsh/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.serg.custonsearsh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+
+
+        
+    }
+}
